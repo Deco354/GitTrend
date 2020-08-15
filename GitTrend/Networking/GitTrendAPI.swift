abd5b80fbd73c25b11f43c7dd978bcc6b482e01a
Declan McKenna <deco354@gmail.com>
2020-08-15 21:05:47 +0100

    Add GitTrendAPI
    Downloads trending repos from [github-trending-api](https://developer.apple.com/library/archive/documentation/Xcode/Reference/xcode_markup_formatting_ref/Links.html)

---
+++ GitTrend/Networking/GitTrendAPI.swift
@@ -0,0 +1,46 @@
+//
+//  GitTrendAPI.swift
+//  GitTrend
+//
+//  Created by Declan McKenna on 15/08/2020.
+//  Copyright © 2020 Declan McKenna. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIImage
+
+/// Downloads trending repos from [github-trending-api](https://developer.apple.com/library/archive/documentation/Xcode/Reference/xcode_markup_formatting_ref/Links.html)
+class GitTrendAPI  {
+    private let session: NetworkSession
+    private let url = URL(string: "https://ghapi.huchen.dev/repositories?language=swift&since=weekly")!
+    
+    init(session: NetworkSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    /// Downloads json from github-trending-api and decodes in to `Repo` array
+    func fetchTrendingRepos(withCompletion completionHandler: @escaping ([Repo]) -> Void) {
+        session.loadData(from: url) { json,error in
+            guard let json = json,
+                error == nil else {
+                    completionHandler([])
+                    print(error ?? "No Data or error found")
+                    return
+            }
+            let repos = self.decode(json: json)
+            completionHandler(repos)
+        }
+    }
+    
+    private func decode(json: Data) -> [Repo] {
+        let decoder = JSONDecoder()
+        do {
+            let rawResponse = try decoder.decode([Repo].self, from: json)
+            return rawResponse
+        } catch(let error) {
+            print(error)
+            return []
+        }
+    }
+}
+
