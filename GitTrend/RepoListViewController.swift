26c141add26923ad1d5f4b917131cf9487b47241
Declan McKenna <deco354@gmail.com>
2020-08-16 19:42:59 +0100

    Create RepoListViewController

---
+++ GitTrend/RepoListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RepoListViewController.swift
+//  GitTrend
+//
+//  Created by Declan McKenna on 15/08/2020.
+//  Copyright © 2020 Declan McKenna. All rights reserved.
+//
+
+import UIKit
+
+/// Displays list of trending repos within `UITableView`
+class RepoListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var repos: [Repo] = [
+        Repo(name: "gvisor", description: "Container Runtime Sandbox", author: "google", url: URL(string: "https://github.com/google/gvisor")!, avatar: URL(string: "https://github.com/google.png")!, stars: 3320)
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension RepoListViewController: UITableViewDelegate {}
+
+extension RepoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell", for: indexPath) as? RepoCell else {
+            fatalError("TableView cast to invalid or non existant type")
+        }
+        configure(cell: cell, at: indexPath)
+        return cell
+    }
+    
+    private func configure(cell: RepoCell, at indexPath: IndexPath) {
+        let repo = repos[indexPath.row]
+        cell.nameLabel.text = repo.name
+        cell.authorLabel.text = repo.author
+        cell.descriptionLabel.text = repo.description
+    }
+}
+
