20b062d871215b82cad1000b9e29e9f034d493c1
Declan McKenna <deco354@gmail.com>
2020-08-16 19:23:57 +0100

    Create Basic RepoCell
    Contains labels for name, description and author. More Views will be added later.

---
+++ GitTrend/RepoCell.swift
@@ -0,0 +1,16 @@
+//
+//  RepoCell.swift
+//  GitTrend
+//
+//  Created by Declan McKenna on 16/08/2020.
+//  Copyright © 2020 Declan McKenna. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+}
