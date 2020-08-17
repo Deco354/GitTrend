c1e67f3107d96fb506feb5c016638c45489507cb
Declan McKenna <deco354@gmail.com>
2020-08-17 14:39:42 +0100

    Add AuthorImage to RepoCell

--- GitTrend/RepoCell.swift
+++ GitTrend/RepoCell.swift
@@ -12,5 +12,6 @@ class RepoCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var authorImage: UIImageView!
     
 }
